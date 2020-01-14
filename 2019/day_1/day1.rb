575557e6952cd71015bdbcbae704c18e364b262d
BertZZ <bertie.wooles@gmail.com>
2020-01-14 22:02:35 +0000

    Cleaned up some leftover artifacts from pre refactor

--- 2019/day_1/day1.rb
+++ 2019/day_1/day1.rb
@@ -1,6 +1,6 @@
 def fuel_required
   result = 0
-  file = File.open("/home/bertzz/Projects/advent_of_code/2019/day_1/input.txt")
+  file = File.open("input.txt")
   data = file.read
   puts data.split("\n").map(&:to_i).inject(0) {|res, n| res + n/3.floor - 2 }
 end
